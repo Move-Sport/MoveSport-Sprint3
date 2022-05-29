83402fceb12ea814a53eb3190a393c220214ae44
IgoMrn <igor.oliveira@sptech.school>
2022-05-29 20:35:48 -0300

    mudando a parte do perfil

--- Site/API_projetoMoveSport/src/database/config.js
+++ Site/API_projetoMoveSport/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     user: "root",
     database: "MoveSport",
-    password: "jujuba123",
+    password: "marutorei125",
 };
 
 function executar(instrucao) {
