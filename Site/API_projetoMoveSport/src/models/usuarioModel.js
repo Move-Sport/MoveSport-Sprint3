57879a4d4d30f6caadc1d1a41a983b87486a6456
RaulMeira <raul.souza@sptech.school>
2022-06-01 19:50:48 -0300

    site final

--- Site/API_projetoMoveSport/src/models/usuarioModel.js
+++ Site/API_projetoMoveSport/src/models/usuarioModel.js
@@ -25,7 +25,7 @@ function cadastrar(nome, cpf, tel,  email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, cpf, tel, email, senha) VALUES ('${nome}', '${cpf}','${tel}','${email}', '${senha}');
+        INSERT INTO usuario (nome, cpf, tel, email, senha) VALUES ('${nome}', '${cpf}','${tel}','${email}', MD5('${senha}'));
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
